26afc770ed4b4f3cfc05560d80a01e1c148e064f
Ubuntu <ubuntu@ip-172-31-45-47.us-east-2.compute.internal>
2025-11-09 15:26:29 +0000

    added telescope mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,3 +9,13 @@ map("i", "jk", "<ESC>")
 map("t", "<ESC>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+local builtin = require("telescope.builtin")
+map("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+map("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+map("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+map("n", "<leader>/", function()
+    builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+        winblend = 10,
+        previewer = false,
+    }))
+end, { desc = "[/] Fuzzily search in current buffer" })
