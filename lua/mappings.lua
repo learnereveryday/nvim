2c8eec54e1527ebee8075e45974043e780f563dd
Ubuntu <ubuntu@ip-172-31-45-47.us-east-2.compute.internal>
2025-11-09 15:01:25 +0000

    added <ESC> for terminal -> normal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("t", "<ESC>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
